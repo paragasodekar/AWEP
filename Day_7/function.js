24a1361bfb61eb5e93d11de9cda2b2e5a1a59908
paragasodekar <65756989+paragasodekar@users.noreply.github.com>
2020-08-13 01:22:22 +0530

    Add files via upload

---
+++ Day_7/function.js
@@ -0,0 +1,24 @@
+// named function
+let abc= function first() {
+    console.log('Hello Named function');
+}
+
+first();
+
+
+// asynonims function
+
+let pqr= function () {
+    console.log('Hello asynonims function');
+}
+
+pqr();
+
+// Lamda or Arrow Functon
+let xyz = ()=>{
+    console.log('Hello arrow function');
+}
+
+xyz();
+
+// Parameterised function
